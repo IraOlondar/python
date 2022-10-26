f7cd30cf2f03697bcdb36499765f57fd08f559cc
IraOlondar <iolondar@yandex.ru>
2022-10-26 22:04:51 +0300

    edit lesson_8/db_organisation/logger.py

--- lesson_8/db_organization/logger.py
+++ lesson_8/db_organization/logger.py
@@ -3,7 +3,7 @@ import logging
 logging.basicConfig(
     level=logging.INFO,
     encoding='utf-8',
-    filename='phone_book.log',
+    filename='data.log',
     format='[%(asctime)s] [%(levelname)s] [%(module)s] [%(funcName)s: %(lineno)d] => %(message)s',
     datefmt='%d.%m.%Y %H:%M:%S ',
 )
